7dc2d5d2d0291c216579287763e39437653cb009
LeeChuang <loe615@gmail.com>
2017-12-18 15:26:42 +0800

    merge data

--- INet.cpp
+++ INet.cpp
@@ -6,10 +6,13 @@ CINet::CINet()
 {
 	m_socket = INVALID_SOCKET;
 	m_net_type = NetType::TCP_CLIENT_ASK_ANSWER;
-	m_link_type = LinkType::KEEP_ALIVE;
 	m_max_client_number = DEFAULT_LINK_NUMBER;
 	m_strIP = DEFAULT_IP;
 	m_port = DEFAULT_PORT;
+}
+CINet::CINet(NetType net_type, std::string ip, UINT port)
+{
+	InitializeNet(net_type, ip, port);
 }
 
 CINet::~CINet()
@@ -34,68 +37,14 @@ void CINet::Shutdown()
 	m_socket = INVALID_SOCKET;
 }
 
-bool CINet::InitializeNet(NetType net_type, LinkType link_type, std::string ip, UINT port)
-{
-	try
-	{
-		WSADATA wsa;
-		ZeroMemory(&wsa, sizeof(WSADATA));
-		if (WSAStartup(MAKEWORD(2, 2), &wsa))
-			return false;
-
-		m_net_type = net_type;
-		m_link_type = link_type;
-		m_strIP = ip;
-		m_port = port;
-
-	}
-	catch (...)
-	{
-		return false;
-	}
-
-	return true;
-};
-
 bool CINet::connecting()
 {
-
-	try
-	{
-		if (m_net_type == UDP)
-			return true;
-		else if (m_net_type == TCP_SERVER)
-		{
-			if ((m_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-				return false;
-		}
-		else
-		{
-			if (m_socket != INVALID_SOCKET)
-				return true;
-
-			if ((m_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-				return false;
-
-			SOCKADDR_IN server_address;
-			server_address.sin_family = AF_INET;
-			server_address.sin_addr.S_un.S_addr = inet_addr(m_strIP.c_str());
-			server_address.sin_port = htons(m_port);
-			memset(server_address.sin_zero, 0, sizeof(server_address.sin_zero));
-
-			if (connect(m_socket, (sockaddr*)&server_address, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
-			{
-				closesocket(m_socket);
-				m_socket = INVALID_SOCKET;
-			}
-		}
-
-	}
-	catch (...)
-	{
-		return false;
-	}
-	return true;
+	if (m_net_type == UDP)
+		return true;
+	else if (m_net_type == TCP_SERVER)
+		return TCPServerConnect();
+	else
+		return TCPClientConnect();
 }
 
 int CINet::SendData(char* str_data)
@@ -103,17 +52,25 @@ int CINet::SendData(char* str_data)
 	if (m_net_type == UDP)
 		return UDPSendData(str_data);
 	else if (m_net_type == TCP_SERVER)
-	{
-
-	}
+		return TCPServerSendData(str_data);
 	else
 		return TCPClientSendData(str_data);
-	
+}
+
+int CINet::ReciveData(char* str_data)
+{
+	if (m_net_type == UDP)
+		return UDPReciveData(str_data);
+	else if (m_net_type == TCP_SERVER)
+		return TCPServerReciveData(str_data);
+	else
+		return TCPClientReciveData(str_data);
 }
 
 int CINet::UDPSendData(char* str_data)
 {
 	int send_size = 0;
+	SOCKET temp_socket = INVALID_SOCKET;
 	try
 	{
 		if ((m_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
@@ -127,19 +84,24 @@ int CINet::UDPSendData(char* str_data)
 		while (send_size != strlen(str_data))
 		{
 			int temp_send_size = 0;
-			temp_send_size = sendto(m_socket, str_data + send_size, strlen(str_data) - send_size, 0, (sockaddr*)&temp_addr, sizeof(SOCKADDR_IN));
+			temp_send_size = sendto(temp_socket, 
+				str_data + send_size, 
+				strlen(str_data) - send_size, 
+				0, 
+				(sockaddr*)&temp_addr, 
+				sizeof(SOCKADDR_IN));
 			if (temp_send_size == SOCKET_ERROR)
 			{
 				send_size = -1;
-				closesocket(m_socket);
-				m_socket = INVALID_SOCKET;
+				closesocket(temp_socket);
+				temp_socket = INVALID_SOCKET;
 				break;
 			}
 			send_size += temp_send_size;
 		}
 		Sleep(20);
-		closesocket(m_socket);
-		m_socket = INVALID_SOCKET;
+		closesocket(temp_socket);
+		temp_socket = INVALID_SOCKET;
 	}
 	catch (...)
 	{
@@ -153,13 +115,19 @@ int CINet::TCPClientSendData(char* str_data)
 	int send_size = 0;
 	try
 	{
-		if(!connecting())
-			return -1;
-
+		if (m_socket == INVALID_SOCKET)
+		{
+			if (!connecting())
+				return -1;
+		}
+		
 		while (send_size != strlen(str_data))
 		{
 			int temp_send_size = 0;
-			temp_send_size = send(m_socket, str_data + send_size, strlen(str_data) - send_size, 0);
+			temp_send_size = send(m_socket, 
+				str_data + send_size, 
+				strlen(str_data) - send_size, 
+				0);
 			if (temp_send_size == SOCKET_ERROR)
 			{
 				send_size = -1;
@@ -169,12 +137,39 @@ int CINet::TCPClientSendData(char* str_data)
 			}
 			send_size += temp_send_size;
 		}
+	}	
+	catch (...)
+	{
+		return -1;
+	}
+	return send_size;
+}
 
-		if (m_link_type == SHORT_CONNECTION)
+int CINet::TCPServerSendData(char* str_data)
+{
+	int send_size = 0;
+	try
+	{
+		if (m_socket == INVALID_SOCKET)
 		{
-			Sleep(20);
-			closesocket(m_socket);
-			m_socket = INVALID_SOCKET;
+			if (!connecting()) return -1;
+		}
+
+		while (send_size != strlen(str_data))
+		{
+			int temp_send_size = 0;
+			temp_send_size = send(m_socket, 
+				str_data + send_size, 
+				strlen(str_data) - send_size, 
+				0);
+			if (temp_send_size == SOCKET_ERROR)
+			{
+				send_size = -1;
+				closesocket(m_socket);
+				m_socket = INVALID_SOCKET;
+				break;
+			}
+			send_size += temp_send_size;
 		}
 	}
 	catch (...)
@@ -182,4 +177,76 @@ int CINet::TCPClientSendData(char* str_data)
 		return -1;
 	}
 	return send_size;
+}
+bool CINet::TCPServerConnect()
+{
+	try
+	{
+		if ((m_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+			return false;
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool CINet::TCPClientConnect()
+{
+	try
+	{
+		if ((m_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+			return false;
+
+		SOCKADDR_IN server_address;
+		server_address.sin_family = AF_INET;
+		server_address.sin_addr.S_un.S_addr = inet_addr(m_strIP.c_str());
+		server_address.sin_port = htons(m_port);
+		memset(server_address.sin_zero, 0, sizeof(server_address.sin_zero));
+
+		if (connect(m_socket, (sockaddr*)&server_address, sizeof(SOCKADDR_IN)) 
+			== SOCKET_ERROR)
+		{
+			closesocket(m_socket);
+			m_socket = INVALID_SOCKET;
+			return false;
+		}
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
+}
+
+int CINet::UDPReciveData(char* str_data)
+{
+	int recv_size = 0;
+
+	return recv_size;
+}
+
+int CINet::TCPServerReciveData(char* str_data)
+{
+	int recv_size = 0;
+
+	return recv_size;
+}
+
+int CINet::TCPClientReciveData(char* str_data)
+{
+	int recv_size = 0;
+	try
+	{
+		if (m_socket == INVALID_SOCKET)
+		{
+			if (!connecting()) return -1;
+		}		
+	}
+	catch (...)
+	{
+		return -1;
+	}
+	return recv_size;
 }
--- INet.h
+++ INet.h
@@ -10,6 +10,7 @@
 #define DEFAULT_LINK_NUMBER 5
 #define DEFAULT_IP "127.0.0.1"
 #define DEFAULT_PORT 45678
+#define UDP_LOCAL_PORT 51515
 // 网络类型
 enum NetType{
 	TCP_CLIENT_ASK_ANSWER,	//TCP客户端 问答模式
@@ -17,21 +18,19 @@ enum NetType{
 	TCP_SERVER,				//TCP服务端模式
 	UDP						//UDP模式
 };
-// 连接类型
-enum LinkType{
-	KEEP_ALIVE,				//长连接
-	SHORT_CONNECTION		//短连接
-};
+//// 连接类型
+//enum LinkType{
+//	KEEP_ALIVE,				//长连接
+//	SHORT_CONNECTION		//短连接
+//};
 class CINet
 {
 public:
 	//构造、析构
 	CINet();
+	CINet(NetType net_type, std::string ip, UINT port);
 	~CINet();
 
-	//初始化网络
-	bool InitializeNet(NetType net_type, LinkType link_type, std::string ip, UINT port);
-	
 	//连接
 	bool connecting();
 
@@ -44,20 +43,42 @@ public:
 	//关闭连接
 	void Shutdown();
 
+	//初始化网络
+	inline void InitializeNet(NetType net_type, std::string ip, UINT port)
+	{
+		m_net_type = net_type;
+		m_strIP = ip;
+		m_port = port;
+	};
+
 	//设置TCP服务端最大连接数
-	void SetMaxClientNumber(int num){ m_max_client_number = num; };
+	inline void SetMaxClientNumber(int num){ m_max_client_number = num; };
+
+	//设置UDP本地监视端口
+	inline void SetUDPLocalPort(UINT port){ m_udp_local_port = port; }
+
 private:
 	int UDPSendData(char *str_data);
 	int TCPClientSendData(char *str_data);
 	int TCPServerSendData(char *str_data);
 
+	bool TCPClientConnect();
+	bool TCPServerConnect();
+
+	int UDPReciveData(char *str_data);
+	int TCPClientReciveData(char *str_data);
+	int TCPServerReciveData(char *str_data);
 private:
 	WSAEVENT m_event;
 	SOCKET m_socket;
 	NetType m_net_type;
-	LinkType m_link_type;
+
+	//目标ip及端口
 	std::string m_strIP;
 	UINT m_port;
+
+	//UDP本地监视端口
+	UINT m_udp_local_port;
 
 	//TCP服务端时  使用数据
 	UINT m_max_client_number;
